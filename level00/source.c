5c1b45ddd8ca55fbebf396e3657a785a660ae55b
eprei <93380002+eprei@users.noreply.github.com>
2025-10-06 10:46:51 +0200

    change level00 source from asm to c

---
+++ level00/source.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool main(void){
+  int user_input [4];
+  
+  puts("***********************************");
+  puts("* \t     -Level00 -\t\t  *");
+  puts("***********************************");
+  printf("Password:");
+  scanf(%d,user_input);
+  if (user_input[0] != 5276) {
+    puts("\nInvalid Password!");
+  }
+  else {
+    puts("\nAuthenticated!");
+    system("/bin/sh");
+  }
+  return user_input[0] != 5276;
+}
